23a80362713ce3ec61daf1970703d79d58bccd6b
Matome <matomemodiba3@gmail.com>
2024-02-03 17:04:23 +0200

    new file

---
+++ app/src/main/java/com/example/basicweatherapp/utilities/Constants.java
@@ -0,0 +1,10 @@
+package com.example.basicweatherapp.utilities;
+
+import java.util.List;
+
+public class Constants {
+
+    public static String API_KEY = "130351bad8084889bb393714222103";
+    public static String PLACES_LIST = "PLACES_LIST";
+    public static String LOCATION = "LOCATION";
+}
